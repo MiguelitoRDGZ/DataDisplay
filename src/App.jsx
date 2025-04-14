87753c4bdad6b83c21c0746b066e884ad3fb7569
MiguelitoRDGZ <itsmiguelrdgz@icloud.com>
2025-04-14 11:49:29 -0400

    asdasd

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import KpiSheet from "./pages/KpiSheet";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <KpiSheet />
+    </div>
+  );
+}
+
+export default App;
